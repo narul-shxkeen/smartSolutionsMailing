8ad072e6698a4e3080510de12ca6a98d8c11c03d
Vaibhav Shokeen <shokeenvaibhav280@gmail.com>
2024-07-21 15:26:04 +0530

    setting up the mailing service for smart solutions print and pack

---
+++ mailingService.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import {config} from "dotenv"; 
+const router = express.Router();
+config();
+const myemail=process.env.email;
+const mypass=process.env.password;
+console.log(myemail, " and ", mypass);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user:myemail,
+      pass: mypass,
+    },
+  });
+  
+  contactEmail.verify((error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Ready to Send");
+    }
+  });
+
+  router.post("/contact", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message; 
+    const mail = {
+      from: name,
+      to:"smartsolutionsprintnpack@gmail.com",
+      subject: "Contact Form Submission From Website",
+      html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json({ status: "ERROR" });
+      } else {
+        res.json({ status: "Message Sent" });
+      }
+    });
+  });
